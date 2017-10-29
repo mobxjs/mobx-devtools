e4e20be561a40b5bcb43a085951a7a2e01102df2
Dan Argue <dargue3@users.noreply.github.com>
2017-10-29 10:38:03 -0700

    Add support for SSR

--- packages/mobx-devtools-mst/src/index.js
+++ packages/mobx-devtools-mst/src/index.js
@@ -1,11 +1,10 @@
 import * as mobx from 'mobx'; // eslint-disable-line
 import * as libmst from 'mobx-state-tree'; // eslint-disable-line
 
-const hook = window.__MOBX_DEVTOOLS_GLOBAL_HOOK__; // eslint-disable-line no-underscore-dangle
-if (hook && hook.inject) hook.inject({ mobx, mst: libmst });
-
-
 const track = (root) => {
+  const hook = window.__MOBX_DEVTOOLS_GLOBAL_HOOK__; // eslint-disable-line no-underscore-dangle
+  if (hook && hook.inject) hook.inject({ mobx, mst: libmst });
+
   if (!hook) return;
   for (const mobxid in hook.collections) {
     if (Object.prototype.hasOwnProperty.call(hook.collections, mobxid)) {
