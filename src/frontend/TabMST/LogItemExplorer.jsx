6e83959d33a2b36dc2be51cffb90887c5c03bab5
Andrey Kogut <mail@andreykogut.com>
2018-02-24 14:05:04 +0200

    mst: track snapshots instead of patches

--- src/frontend/TabMST/LogItemExplorer.jsx
+++ src/frontend/TabMST/LogItemExplorer.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import injectStores from '../../utils/injectStores';
 import DataViewer from '../DataViewer';
 import Collapsible from '../Collapsible';
+import PreviewValue from '../PreviewValue';
 
 @injectStores({
   subscribe: ({ mstLoggerStore }) => {
@@ -55,14 +56,22 @@ export default class LogItemExplorer extends React.PureComponent {
             />
           </Collapsible>
         }
-        {this.props.logItem.patch && !this.props.initial &&
-          <Collapsible head={'Patch'} startOpen>
-            <DataViewer
-              path={['patch']}
-              getValueByPath={this.props.getValueByPath}
-              decorator={this.dataDecorator}
-            />
-          </Collapsible>
+        {this.props.logItem.patches && !this.props.initial && (
+          <div className={css(styles.patches)}>
+            {this.props.logItem.patches.map((patch) => {
+              const path = patch.path.replace(/^\//, '').replace(/\//g, '.');
+              switch (patch.op) {
+                case 'remove':
+                  return (
+                    <div>{path} <span className={css(styles.removedLabel)}>Removed</span></div>
+                  );
+                default: return (
+                  <div>{path} = <PreviewValue data={patch.value} /></div>
+                );
+              }
+            })}
+          </div>
+        )
         }
       </div>
     );
@@ -71,7 +80,17 @@ export default class LogItemExplorer extends React.PureComponent {
 
 const styles = StyleSheet.create({
   logExplorer: {
+    flex: '1 1 auto',
     padding: 5,
     overflow: 'auto',
+  },
+  patches: {
+    marginTop: 20,
+    paddingLeft: 15,
+  },
+  removedLabel: {
+    textTransform: 'uppercase',
+    fontSize: 10,
+    color: '#c41a16',
   },
 });
