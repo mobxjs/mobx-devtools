8a461e9457472f7152fe613ff1091d21ecc0a449
Tyler Scott Williams <tyler@coolsoftware.dev>
2024-12-04 07:07:20 -0700

    fix: global hook not installing (#133)

--- src/shells/webextension/injectGlobalHook.js
+++ src/shells/webextension/injectGlobalHook.js
@@ -1,9 +1,11 @@
 // Simply importing this file will install the global hook here
 import installGlobalHook from '../../backend/utils/installGlobalHook';
 
-// if (__DEV__) {
-window.addEventListener('test-open-mobx-devtools-window', () => {
-  console.log('test-open-mobx-devtools-window'); // eslint-disable-line no-console
-  chrome.runtime.sendMessage({ eventName: 'open-mobx-devtools-window' });
-});
-// }
+if (__DEV__) {
+  window.addEventListener('test-open-mobx-devtools-window', () => {
+    console.log('test-open-mobx-devtools-window'); // eslint-disable-line no-console
+    chrome.runtime.sendMessage({ eventName: 'open-mobx-devtools-window' });
+  });
+}
+
+installGlobalHook(window);
